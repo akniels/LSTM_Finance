dc7e734090d7dccfd6de152b910950f6f062b4e3
Andrew Nielson <andrew.kyle.nielson@gmail.com>
2021-02-15 16:05:28 -0700

    Update etl.py

--- etl.py
+++ etl.py
@@ -10,69 +10,29 @@ import  yfinance as yf
 import pandas as pd
 
 
-finance_tick = gt.get_tickers_filtered(sectors=gt.SectorConstants.FINANCE)
-finance_tick = ' '.join(finance_tick)
-
-data = yf.download(finance_tick[0], start="2014-01-01", end="2020-01-01" ,group_by='ticker')
-data['ticker'] = finance_tick[0]
-
-
 class download_data:
     
     
     def Download(): 
-    
-    
-        finance_tick = gt.gt.get_tickers_filtered(sectors=gt.SectorConstants.FINANCE)
-        #finance_tick = ' '.join(finance_tick)
-        
-        tec_tick = gt.get_tickers_filtered(sectors=gt.SectorConstants.TECH)
-        #tec_tick = ' '.join(tec_tick)
-        
-        
-        energy_tick = gt.get_tickers_filtered(sectors=gt.SectorConstants.ENERGY)
-        #energy_tick = ' '.join(energy_tick)
-    
-    
-    
-        data = yf.download(finance_tick[0], start="2014-01-01", end="2020-01-01" ,group_by='Ticker')
-        data['ticker'] = finance_tick[0]
-        for item in finance_tick[1:]:
+        nasdaq = pd.read_csv('nasdaq_screener.csv')
+        # change to list of tickers
+        tickers = [i for i in nasdaq['Symbol']]
+        # download first row of data
+        data = yf.download(tickers[0], start="2014-01-01", end="2021-01-01" ,group_by='Ticker')
+        # create row for ticker
+        data['ticker'] = tickers[0]
+        # for loop to append all other rows to initial one
+        for item in tickers[1:]:
             try:
-                dat = yf.download(item, start="2014-01-01", end="2020-01-01" ,group_by='ticker')
+                dat = yf.download(item, start="2014-01-01", end="2021-01-01" ,group_by='ticker')
             except ValueError:
                 continue
             dat['ticker'] = item
             if dat.shape[0] == 0:
                 continue
             data  = pd.concat([data, dat])
-        data.to_csv('Finance.csv') 
-    
-        tec_data = yf.download(tec_tick[0], start="2014-01-01", end="2020-01-01" ,group_by='Ticker')
-        tec_data['ticker'] = tec_tick[0]
-        for item in energy_tick[1:]:
-            try:
-                tdat = yf.download(item, start="2014-01-01", end="2020-01-01" ,group_by='ticker')
-            except ValueError:
-                continue
-            tdat['ticker'] = item
-            if tdat.shape[0] == 0:
-                continue
-            data  = pd.concat([tec_data, tdat])
-        tec_data.to_csv('Tech.csv') 
-    
-        energy_data = yf.download(energy_tick[0], start="2014-01-01", end="2020-01-01" ,group_by='Ticker')
-        energy_data['ticker'] = energy_tick[0]
-        for item in energy_tick[1:]:
-            try:
-                edat = yf.download(item, start="2014-01-01", end="2020-01-01" ,group_by='ticker')
-            except ValueError:
-                continue
-            edat['ticker'] = item
-            if edat.shape[0] == 0:
-                continue
-            data  = pd.concat([energy_data, edat])
-        energy_data.to_csv('Energy.csv') 
+        # write to csv
+        data.to_csv('nasdaq.csv') 
 
 if __name__ == "__main__":
     download_data.Download()
